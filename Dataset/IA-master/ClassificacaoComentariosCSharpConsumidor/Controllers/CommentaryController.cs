8148eadb61b86fe545be2957fa3c214d5315bf29
Sakthyvell <sakthyvellsuperamaniam@gmail.com>
2019-09-26 07:20:31 +0800

    new files

---
+++ Dataset/IA-master/ClassificacaoComentariosCSharpConsumidor/Controllers/CommentaryController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassificacaoComentariosCSharpConsumidor.Types;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML;
+
+namespace ClassificacaoComentariosCSharpConsumidor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentaryController : ControllerBase
+    {
+        [HttpGet("{text}")]
+        public ActionResult<PredictionResult> Get(string text)
+        {
+            string diretorioModelo = "Model\\model.zip";
+
+            MLContext mlContext = new MLContext();
+            ITransformer model;
+
+            using (var stream = new FileStream(diretorioModelo, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                model = mlContext.Model.Load(stream, out DataViewSchema modelSchema);
+            }
+
+            var predictiveModel = 
+                mlContext.Model.CreatePredictionEngine<Commentary, PredictionResult>(model);
+
+            return predictiveModel.Predict(new Commentary()
+            {
+                Text = text
+            });
+        }
+
+    }
+}
---
+++ Dataset/IA-master/ClassificacaoComentariosCSharpConsumidor/Types/PredictionResult.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.ML.Data;
+
+namespace ClassificacaoComentariosCSharpConsumidor.Types
+{
+    public class PredictionResult
+    {
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+        public float Score { get; set; }
+    }
+}
